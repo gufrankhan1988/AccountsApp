7f71d9870d3bcc54aaed23b634e1835457c0c5e0
gufran1.4u@gmail.com <turniton96160>
2017-08-05 01:28:35 +0530

    New code

---
+++ Entities/Stock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Stock
+    {
+        public int StockID { get; set; }
+        public Item item { get; set; }
+        public UnitType Unit { get; set; }
+
+    }
+}
